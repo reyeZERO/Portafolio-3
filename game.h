f0db8d63abf5c737e7559d08cfdd83dbb702db86
reyeZERO <reyescadenaj28@gmail.com>
2025-05-11 23:30:44 -0500

    Create game.h

---
+++ game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+#include "rules.h"
+
+class GameOfLife {
+public:
+    GameOfLife();
+    void configureRules();
+    void initialize();
+    bool run();
+    void update();
+    void display();
+
+private:
+    std::vector<std::vector<bool>> grid;
+    std::vector<std::vector<bool>> nextGrid;
+    Rules rules;
+    int rows;
+    int cols;
+    int generation;
+    bool isRunning;
+
+    int countNeighbors(int x, int y);
+    void clearScreen();
+};
+
+#endif
